55efbf69c6bd69b41f37ad01170319e968d2be0d
SalvaChLl <salvachll@gmail.com>
2020-12-29 00:03:46 +0100

    some change

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const scrap = require("./scrap");
 const database = require("knex")({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: process.env.DB_PASS,
-    database: "sondeo_practicas",
+    host: "ec2-18-211-171-122.compute-1.amazonaws.com",
+    user: "gxojsjqdznsveg",
+    password: "f850014133cf479b5b628004445235fe7d20c9302857b95c6f315e8044e2fe5a",
+    database: "d8gtoui997bn8c",
   },
 });
 
